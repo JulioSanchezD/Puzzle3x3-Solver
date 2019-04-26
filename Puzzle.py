27c9b40aebbe1a28bc898bd15e7ffc718f188412
Julio SanchezD <julioe.sanchezd@gmail.com>
2019-04-25 19:32:28 -0500

    Added a method for expanding a node to the right direction

--- Puzzle.py
+++ Puzzle.py
@@ -1,15 +1,20 @@
 class Puzzle:
 
-    def __init__(self, puzzle, goal, heuristic="hamming"):
-        self.puzzle = puzzle
+    def __init__(self, initial_state, goal, heuristic="hamming"):
+        self.initial_state = initial_state
         self.goal = goal
         self.heuristic = heuristic
 
     def __str__(self):
         string = ""
-        for index, row in enumerate(self.puzzle):
+        for index, row in enumerate(self.initial_state):
             string += str(row) + "  " + str(self.goal[index]) + "\n"
         return string
+
+    @staticmethod
+    def print(grid):
+        for row in grid:
+            print(row)
 
     @staticmethod
     def getPosition(x, grid):
@@ -18,23 +23,34 @@ class Puzzle:
                 if num == x:
                     return (row_num, col_num)
 
-    def getCost(self):
+    def getCost(self, grid):
         cost = 0
         if self.heuristic == "hamming":
             for i in range(0, 3):
                 for j in range(0, 3):
-                    if self.puzzle[i][j] != self.goal[i][j]:
+                    if grid[i][j] != self.goal[i][j]:
                         cost += 1
         else:
             for r1 in range(0, 3):
                 for c1 in range(0, 3):
-                    r2, c2 = self.getPosition(self.puzzle[r1][c1], self.goal)
+                    r2, c2 = self.getPosition(grid[r1][c1], self.goal)
                     cost += abs(r2 - r1) + abs(c2 - c1)
         return cost
 
+    def moveRight(self, grid):
+        row, col = self.getPosition(0, grid)
+        if col == 2:
+            return False
+        else:
+            val = grid[row][col+1]
+            grid[row][col] = val
+            grid[row][col+1] = 0
+            return grid 
 
 if __name__ == "__main__":
     grid = [[2, 8, 3], [1, 0, 4], [7, 6, 5]]
     goal = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
     puzzle = Puzzle(grid, goal, "manhattan")
-    print(puzzle.getCost())
+    print(str(puzzle))
+    node = puzzle.moveRight(puzzle.initial_state)
+    puzzle.print(node)
